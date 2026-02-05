85abbbdc706f96daff804491e0078dbdc9ee6ed1
esvet1 <65078624+esvet1@users.noreply.github.com>
2026-02-05 22:25:44 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     domains: ['localhost', '*.supabase.co'],
     formats: ['image/webp', 'image/avif'],
